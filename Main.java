f74d4eaa3c1069bbb5525a5cb060aba52ffd0d1e
DannyDeutsch <danielbdeutsch@gmail.com>
2016-04-30 15:01:58 -0400

    updates

--- Main.java
+++ Main.java
@@ -13,26 +13,27 @@ public class Main
             System.exit(0);
         }
 
-        Database dbTrain, dbTest = null;
+        Database dbTrain = null;
+        Database dbTest = null;
         try {
-            dbTest  = new Database(args[1]);    //2012
-            // dbTrain = new Database(args[0]);    //2016
+            dbTrain = new Database(args[0]);    //2016
+            // dbTest  = new Database(args[1]);    //2012
         } catch (Exception e) {
             System.out.println("Err2: Could not create new Database object");
             System.exit(0);
         }
 
 
-        for (int row = 0; row < dbTest.data.size(); row++) {
-            for (int col = 0; col < dbTest.data.get(row).size(); col++) {
-                System.out.print(dbTest.data.get(row).get(col) + "  ");
+        for (int row = 0; row < dbTrain.data.size(); row++) {
+            for (int col = 0; col < dbTrain.data.get(row).size(); col++) {
+                System.out.print(dbTrain.data.get(row).get(col) + "  ");
             }
 
             System.out.println();
         }
 
-        System.out.println("\nCols: " + dbTest.data.get(0).size());
-        System.out.println("Rows: " + dbTest.data.size());
+        System.out.println("\nCols: " + dbTrain.data.get(0).size());
+        System.out.println("Rows: " + dbTrain.data.size());
 
 
 
